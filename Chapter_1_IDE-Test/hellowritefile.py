65a94e2a5d913f544284684d07653489d3c9b4cc
Maria <maria.dermit@qmul.ac.uk>
2021-04-16 14:19:58 +0100

    edits on writefile

--- Chapter_1_IDE-Test/hellowritefile.py
+++ Chapter_1_IDE-Test/hellowritefile.py
@@ -1,9 +1,16 @@
 import sys
+import logging
+
+logging.basicConfig(filename='Chapter_1_IDE-Test/hello_write_logfile.log',level=logging.INFO)
+logging.debug('This message should go to the log file if level is set to DEBUG')
+logging.info('This message only should go to the log file if level is set to INFO')
+
+
 output_file = sys.argv[1]
 output_prefix = sys.argv[2]
 
-print(f"The program: {sys.argv[0]} will write to {output_file}{output_prefix}")
-
+#note that the executed file will be printed out in capital letters
+print(f"The program: {sys.argv[0].upper()} will write to {output_file}{output_prefix}")
 
 def write_expected(output_file, output_prefix):
     output_file_final = output_file + output_prefix
@@ -15,4 +22,4 @@ def write_expected(output_file, output_prefix):
 write_expected(output_file, output_prefix)
 
 
-#python3 Chapter_1_IDE-Test/hellowritefile.py demofile2 test_1
+#python3 Chapter_1_IDE-Test/hellowritefile.py Chapter_1_IDE-Test/demofile2 test_1
