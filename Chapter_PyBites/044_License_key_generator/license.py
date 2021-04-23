e8377fc5b39a90753aac16b28df831e7c957a2d1
Maria <maria.dermit@qmul.ac.uk>
2021-04-23 08:39:50 +0100

    adding secret license on blocks

---
+++ Chapter_PyBites/044_License_key_generator/license.py
@@ -0,0 +1,7 @@
+from secrets import token_hex
+
+#CHARACTERS = (string.ascii_uppercase + string.digits)
+
+
+def gen_key(parts=4, chars_per_part=8):
+    return '-'.join(token_hex(chars_per_part//2) for _ in range(parts)).upper()
