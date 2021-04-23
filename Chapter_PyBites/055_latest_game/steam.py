3b79426057ae10e1329aa807efd37b821947d07a
Maria <maria.dermit@qmul.ac.uk>
2021-04-23 11:20:35 +0100

    steam

--- Chapter_PyBites/055_latest_game/steam.py
+++ Chapter_PyBites/055_latest_game/steam.py
@@ -11,11 +11,11 @@ FEED_URL = "https://bites-data.s3.us-east-2.amazonaws.com/steam_gaming.xml"
 #<class 'str'>
 
 #1) easy way to do this. Parses Steam's RSS feed and prints output to screen
-feed = feedparser.parse(FEED_URL)
-type(feed) #<class 'feedparser.util.FeedParserDict'>
-type(feed.entries) #list 
-for entry in feed.entries:
-    print(entry.title + entry.link)
+# feed = feedparser.parse(FEED_URL)
+# type(feed) #<class 'feedparser.util.FeedParserDict'>
+# type(feed.entries) #list 
+# for entry in feed.entries:
+#     print(entry.title + entry.link)
 
 #2) we can instead return a list of Game named tuples
 
