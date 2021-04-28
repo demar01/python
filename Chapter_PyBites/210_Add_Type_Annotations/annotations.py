2fd79cdf11aa713b47f04206ff21ccf13764c0ac
Maria <maria.dermit@qmul.ac.uk>
2021-04-28 06:22:21 +0100

    annotation

---
+++ Chapter_PyBites/210_Add_Type_Annotations/annotations.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def sum_numbers(numbers: List[int]) -> int:
+    """Sums numbers
+    :param numbers: list of numbers
+    :type numbers: list
+    ...
+    :raises TypeError: if not all values passed in are numeric
+    ...
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
+
+#https://realpython.com/python-type-checking/
+
+#sum_numbers.__annotations__ {'numbers': typing.List[int], 'return': <class 'int'>}
