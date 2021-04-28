810a1846e4d426cf5d9e349c69f9d0b1dbf517de
Maria <maria.dermit@qmul.ac.uk>
2021-04-28 06:56:57 +0100

    fibonacci

---
+++ Chapter_PyBites/238_Fibonacci_test/fibonacci_test.py
@@ -0,0 +1,38 @@
+#1 Recursion
+
+def Fibonacci(n):
+       
+    # Check if input is 0 then it will
+    # print incorrect input
+    if n < 0:
+        print("Incorrect input")
+ 
+    # Check if n is 0
+    # then it will return 0
+    elif n == 0:
+        return 0
+ 
+    # Check if n is 1,2
+    # it will return 1
+    elif n == 1 or n == 2:
+        return 1
+ 
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+ 
+
+# Driver Program
+#print(Fibonacci(9))
+
+#Note that I did not do any cache https://stackoverflow.com/questions/494594/how-to-write-the-fibonacci-sequence
+
+#2) Generator
+
+def fib():
+    a, b = 0, 1
+    while True:            # First iteration:
+        yield a            # yield 0 to start with and then
+        a, b = b, a + b    # a will now be 1, and b will also be 1, (0 + 1)
+
+for index, fibonacci_number in zip(range(10), fib()):
+     print('{i:3}: {f:3}'.format(i=index, f=fibonacci_number))
