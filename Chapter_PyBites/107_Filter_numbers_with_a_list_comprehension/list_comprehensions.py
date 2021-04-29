8fdf89e3ebf83cefb95a16366128900f4ee0e88f
Maria <maria.dermit@qmul.ac.uk>
2021-04-29 08:20:13 +0100

    from collections import namedtuple

--- Chapter_PyBites/107_Filter_numbers_with_a_list_comprehension/list_comprehensions.py
+++ Chapter_PyBites/107_Filter_numbers_with_a_list_comprehension/list_comprehensions.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 numbers= [1,2,3,4]
 
 #easier to create two functions first and then use them in the list comprehension
@@ -8,10 +10,13 @@ def is_even(num):
 def is_positive(num):
     return num > 0
 
-def filter_positive_even_numbers(numbers):
-    return [number for number in numbers if is_even(number) and is_positive(number)]
+BeltStats = namedtuple('BeltStats', 'score ninjas')
 
 ninja_belts = {'yellow': BeltStats(50, 11),
                'orange': BeltStats(100, 7),
                'green': BeltStats(175, 1),
                'blue': BeltStats(250, 5)}
+
+def filter_positive_even_numbers(numbers):
+    return [number for number in numbers if is_even(number) and is_positive(number)]
+
