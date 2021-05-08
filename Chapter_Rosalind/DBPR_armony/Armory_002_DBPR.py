bc919bc993f3cd36969bc12a74928b99f3cfa697
Maria <maria.dermit@qmul.ac.uk>
2021-05-08 16:32:19 +0100

    DBPR

---
+++ Chapter_Rosalind/DBPR_armony/Armory_002_DBPR.py
@@ -0,0 +1,22 @@
+from Bio import ExPASy
+from Bio import SwissProt
+
+#See the docs https://biopython.org/docs/1.75/api/Bio.SwissProt.html
+
+
+def getgo(id):
+	handle = ExPASy.get_sprot_raw(id)
+	record = SwissProt.read(handle)
+	go = [r[2].split(":")[1] for r in record.cross_references if r[0] == "GO" and r[2].startswith('P')]
+	print "\n".join(go)
+
+# Test
+getgo("Q65N65")
+
+
+# id="Q5SLP9"
+# handle = ExPASy.get_sprot_raw(id) #<_io.TextIOWrapper encoding='UTF-8'>
+# record = SwissProt.read(handle) #<Bio.SwissProt.Record object at 0x109873be0>
+
+# [r for r in record.cross_references]
+# go=([r[2].split(":")[1] for r in record.cross_references if r[0] == "GO" and r[2].startswith('P')])
