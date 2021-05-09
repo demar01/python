6dcf087b1c444c31d953d29e1bb3e6b1717c4327
Maria <maria.dermit@qmul.ac.uk>
2021-05-09 09:46:22 +0100

    creating  and checking exceptions

---
+++ Chapter_PyBites/012_User_validation/readme.md
@@ -0,0 +1,14 @@
+# Bite 12. Write a user validation function
+
+## Description
+
+Create a function that takes a username and checks for a valid user:
+
+The username is in USERS,
+the user is not expired, and
+the user has the ADMIN role.
+If those 3 conditions are met return SECRET.
+
+If one of the conditions is not True raise an exception you define yourself: UserDoesNotExist, UserAccessExpired and UserNoPermission respectively. Check out the tests for more detail.
+
+Have fun and keep calm and code in Python!
---
+++ Chapter_PyBites/012_User_validation/validate.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+
+User = namedtuple('User', 'name role expired')
+USER, ADMIN = 'user', 'admin'
+SECRET = 'I am a very secret token'
+
+julian = User(name='Julian', role=USER, expired=False)
+bob = User(name='Bob', role=USER, expired=True)
+pybites = User(name='PyBites', role=ADMIN, expired=False)
+USERS = (julian, bob, pybites)
+
+# define exception classes here
+class UserDoesNotExist(Exception):
+    pass
+
+
+class UserAccessExpired(Exception):
+    pass
+
+
+class UserNoPermission(Exception):
+    pass
+
+
+def get_secret_token(username):
+    for user in USERS:
+        if username == user.name:
+            if user.expired:
+                raise UserAccessExpired
+            if user.role != ADMIN:
+                raise UserNoPermission
+            return SECRET
+    raise UserDoesNotExist
+
+
+
+
+
+
+# This will work but no exceptions are raised
+# def get_secret_token(username):
+#     if username in USERS and username[2] == False and username[1] == ADMIN:
+#         return SECRET
