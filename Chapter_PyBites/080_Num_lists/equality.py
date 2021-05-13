e746bfcd6c1ada13793fb0bcb4a4c4f14339e488
Maria <maria.dermit@qmul.ac.uk>
2021-05-13 06:30:23 +0100

    num equality of two lists

---
+++ Chapter_PyBites/080_Num_lists/equality.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class Equality(Enum):
+    SAME_REFERENCE = 4
+    SAME_ORDERED = 3
+    SAME_UNORDERED = 2
+    SAME_UNORDERED_DEDUPED = 1
+    NO_EQUALITY = 0
+
+
+def check_equality(list1, list2):
+    if list1 is list2:
+        return Equality.SAME_REFERENCE
+    elif list1 == list2:
+        return Equality.SAME_ORDERED
+    elif sorted(list1) == sorted(list2):
+        return Equality.SAME_UNORDERED
+    elif set(list1) == set(list2):
+        return Equality.SAME_UNORDERED_DEDUPED
+    
+    return Equality.NO_EQUALITY
