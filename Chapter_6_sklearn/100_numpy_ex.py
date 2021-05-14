20404e0d37abd30099516f5222899c2be889eaf8
Maria <maria.dermit@qmul.ac.uk>
2021-05-14 08:46:39 +0100

    numpy up 38

--- Chapter_6_sklearn/100_numpy_ex.py
+++ Chapter_6_sklearn/100_numpy_ex.py
@@ -14,3 +14,34 @@ Z = np.diag(1+np.arange(4),k=-1) # 5x5 matrix with values 1,2,3,4 just below the
 print(Z) 
 
 
+Z = np.ones((5,3)) @ np.ones((3,2)) #matrix multiplication
+print(Z)
+
+Z = np.arange(11)
+Z[(3 < Z) & (Z < 8)] *= -1 #change sing in this range
+print(Z)
+
+Z1 = np.random.randint(0,10,10)
+Z2 = np.random.randint(0,10,10)
+print(np.intersect1d(Z1,Z2)) # find common values
+
+np.datetime64('today') + np.timedelta64(1)# numpy can also be used for dates that can be used in arrays
+
+
+A = np.ones(3)*1
+B = np.ones(3)*2
+np.add(A,B,out=B) #will store it in B, without making a copy.
+
+#5x5 matrix with row values ranging from 0 to 4
+
+Z = np.zeros((5,5))
+Z += np.arange(5)
+print(Z)
+
+# without broadcasting
+Z = np.tile(np.arange(0, 5), (5,1))
+print(Z)
+
+#sum a small array faster than np.sum
+Z = np.arange(10)
+np.add.reduce(Z)
