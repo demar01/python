8a4261af4102e4b7b89629f084ae522d130b91de
Maria <maria.dermit@qmul.ac.uk>
2021-05-17 11:46:47 +0100

    maketrans translate

---
+++ Chapter_PyBites/068_Remove_punctuation/clean.py
@@ -0,0 +1,14 @@
+def remove_punctuation(input_string):
+    """Return a str with punctuation chars stripped out"""
+    table = str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    return input_string.translate(table)
+
+
+# txt = "Good night Sam!"
+# x = "mSa"
+# y = "eJo"
+# z = "odnght"
+# mytable = txt.maketrans(x, y, z)
+# print(txt.translate(mytable))
+
+# remeber the pairing maketrans and translate in https://www.w3schools.com/python/ref_string_maketrans.asp
