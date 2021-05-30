f5f41942e552f89d3f6c426d010abec591649a21
Maria Dermit <maria.dermit@qmul.ac.uk>
2021-05-30 08:51:14 +0100

    traversal trees

--- Chapter_3_Useful_functions/max_depth_tree.py
+++ Chapter_3_Useful_functions/max_depth_tree.py
@@ -1,7 +1,14 @@
 #https://www.educative.io/edpresso/finding-the-maximum-depth-of-a-binary-tree
+#https://codeworkshop.dev/video-workshops/2020-05-07-how-to-traverse-a-binary-tree-in-python/
+
+#     1
+#   /   \
+#  2     2
+# /     
+#3
 class Node: 
-  def __init__(self , val): 
-      self.value = val  
+  def __init__(self , value): 
+      self.value = value 
       self.left = None
       self.right = None
 
@@ -22,10 +29,38 @@ def maxDepth(root):
 # Driver code
 root = Node(1)
 root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.right.left = Node(5)
-root.right.right = Node(6)
-root.right.right.left = Node(8)
-root.right.left.right = Node(7)
+root.left.left = Node(3)
+root.right = Node(2)
 print("The max depth is:", maxDepth(root))
+
+# Preorder traversal returns [1,2,3,2]
+def depth_first_traversal_pre(node):
+    if node == None:
+        return
+    print(node.value) #printing the node before we call left and rigth
+    depth_first_traversal_pre(node.left)
+    depth_first_traversal_pre(node.right)
+
+depth_first_traversal_pre(root)
+
+#in order transversal returns [3,2,1,2]
+def depth_first_traversal_io(node):
+    if node == None:
+        return
+    depth_first_traversal_io(node.left)
+    print(node.value) #visiting the left branch and go all the way down before we print out the value
+    depth_first_traversal_io(node.right)
+
+depth_first_traversal_io(root)
+
+#post order transversal returns [3,2,2,1]
+
+def depth_first_traversal_po(node):
+    if node == None:
+        return
+    depth_first_traversal_po(node.left)
+    depth_first_traversal_po(node.right)
+    print(node.value) #operating on the value last
+
+    
+depth_first_traversal_po(root)
