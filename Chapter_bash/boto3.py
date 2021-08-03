c6e56a0eff39914340f7d81dd536686c3f65d4fa
Maria Dermit <maria.dermit@qmul.ac.uk>
2021-08-03 07:42:44 +0100

    boto3 datacamp

---
+++ Chapter_bash/boto3.py
@@ -0,0 +1,42 @@
+# Generate the boto3 client for interacting with S3
+s3 = boto3.client('s3', region_name='us-east-1', 
+                        # Set up AWS credentials 
+                  		aws_access_key_id=AWS_KEY_ID, 
+                        aws_secret_access_key=AWS_SECRET)
+# List the buckets
+buckets = s3.list_buckets()
+
+# Print the buckets
+print(buckets)
+
+
+# Generate the boto3 client for interacting with S3 and SNS
+s3 = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+sns = boto3.client('sns', region_name='us-east-1', 
+                           aws_access_key_id=AWS_KEY_ID, 
+                           aws_secret_access_key=AWS_SECRET)
+
+# List S3 buckets and SNS topics
+buckets = s3.list_buckets()
+topics = sns.list_topics()
+
+# Print out the list of SNS topics
+print(topics)
+
+import boto3
+
+# Create boto3 client to S3
+s3 = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+# Create the buckets
+response_staging = s3.create_bucket(Bucket='gim-staging')
+response_processed = s3.create_bucket(Bucket='gim-processed')
+response_test = s3.create_bucket(Bucket='gim-test')
+
+# Print out the response
+print(response_staging)
