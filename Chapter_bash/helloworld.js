a2de3f92978fc5c9e72e439e9765650f1863d94d
Maria Dermit <maria.dermit@qmul.ac.uk>
2021-08-17 07:15:25 +0200

    javascript

--- Chapter_bash/helloworld.js
+++ Chapter_bash/helloworld.js
@@ -2,4 +2,8 @@ console.log("Hello World");
 console.log(5); 
 /* This is all commented
 This is not going to run 
-•/
+*/
+console.log('Hello' + ' '+ 'World');
+console.log('Hello'.length); 
+
+const foo= 'Hello world'
